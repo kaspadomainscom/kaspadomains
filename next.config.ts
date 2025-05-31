846dd55e889c46e4b7ed39796e0102b85fae6889
undergroundBlock <107368307+undergroundBlock@users.noreply.github.com>
2025-05-31 18:01:32 +0300

    Initial commit 6

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,19 @@
 // next.config.ts
 import type { NextConfig } from "next";
 
+// const cspHeader = `
+//     default-src 'self';
+//     script-src 'self' 'unsafe-eval' 'unsafe-inline';
+//     style-src 'self' 'unsafe-inline';
+//     img-src 'self' blob: data:;
+//     font-src 'self';
+//     object-src 'none';
+//     base-uri 'self';
+//     form-action 'self';
+//     frame-ancestors 'none';
+//     upgrade-insecure-requests;
+//     `;
+
 const nextConfig: NextConfig = {
   output: 'export', // Enable static HTML export
   distDir: 'out',   // Directory GitHub Pages will serve from
