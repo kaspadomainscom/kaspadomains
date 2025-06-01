99739c7e7db94a2ac5f329c36ba465072be9b6de
undergroundBlock <107368307+undergroundBlock@users.noreply.github.com>
2025-06-01 03:08:44 +0300

    Initial commit 6

---
+++ src/data/categories/trending.ts
@@ -0,0 +1,10 @@
+export const trendingDomains = [
+  {
+    name: "trending.kas",
+    listed: true,
+    price: 1500,
+    sellerTelegram: "@kaspadomainscom",
+    kaspaLink: "https://kaspa.com/domain/trending.kas",
+  },
+  // more domains...
+];
